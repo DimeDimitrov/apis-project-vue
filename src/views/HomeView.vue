bf76bf442d6c8955beeefba9df6e2e0e518ed9ac
unknown <129460051+DimeDimitrov@users.noreply.github.com>
2023-05-24 17:09:06 +0200

    Style Home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,9 @@ export default {}
 </script>
 <template lang="">
   <div class="flex items-center justify-center">
-    <div class="title text-4xl">Welcome to my APIs Project using Vue and Tailwind CSS!</div>
+    <div class="title text-4xl text-white mt-40 flex justify-center items-center">
+      Welcome to my APIs Project using Vue and Tailwind CSS!
+    </div>
   </div>
 </template>
 <style lang=""></style>
